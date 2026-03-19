43a87d372e9c7108aa45229d5a1a104797100976
ZHANG YIXIAO <268322958+bosskyj86@users.noreply.github.com>
2026-03-19 15:44:33 +0800

    Upload complete working src/ folder (all functions are available)

---
+++ app.js
@@ -0,0 +1,137 @@
+[
+  {
+    "id": "001",
+    "name": "Koi Kei Bakery",
+    "location": "South 50 meters, Gao Mei Shi Street, Sé District",
+    "average_cost_mop": "140",
+    "recommended_dishes": "Almond Snowflake Crisp",
+    "business_hours": "10:00-22:00",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "002",
+    "name": "Heung Kitchen",
+    "location": "Units I,J,K,L,M, Ground Floor & 1st Floor, Diamond Residence, Nos.388-A to 364-C, Avenida de Horta e Costa, Sé District",
+    "average_cost_mop": "95",
+    "recommended_dishes": "roast chicken",
+    "business_hours": "08:00-22:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "003",
+    "name": "Wu Yi Guang Chao Fu Noodle Restaurant (Lixin Building Branch)",
+    "location": "Shop H, Ground Floor, Lixin Building, No.65, Rua de Nagasaki, Porto Novo, Sé District",
+    "average_cost_mop": "39",
+    "recommended_dishes": "Oyster and Pork Chop Noodle Soup",
+    "business_hours": "weekdays:08:00-19:30; weekend:09:30-19:30",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "004",
+    "name": "Vietnam Pho(NAPE Branch)",
+    "location": "Shop F, Ground Floor, No.731, Avenida da Amizade, Sé District",
+    "average_cost_mop": "63",
+    "recommended_dishes": "Hue Beef Noodle",
+    "business_hours": "10:00-24:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "005",
+    "name": "Zone de Aterros do Porto Exterior PouGwokCaan",
+    "location": "Floor L, Chong Yu Building, 606H‑606G Praça de Infante D. Henrique, Porto Novo, Sé District",
+    "average_cost_mop": "182",
+    "recommended_dishes": "Portuguese-style Baked Oxtail",
+    "business_hours": "11:30-23:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "006",
+    "name": "King of Dumplings Loyal Success(Rio Branch)",
+    "location": "Shop G & H, Ground Floor, Nam Un Garden, No.40-A, Avenida de Horta e Costa, Sé District",
+    "average_cost_mop": "95",
+    "recommended_dishes": "Dumpling, Sichuan Spicy Blood Curd & Offal Casserole",
+    "business_hours": "11:00-23:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "007",
+    "name": "Cafe de Coral(Rio Hotal)",
+    "location": "Ground Floor, Rio Hotel, Nos.25 to 67H, Avenida de Horta e Costa, Porto Novo, Sé District",
+    "average_cost_mop": "36",
+    "recommended_dishes": "Pork Chop Rice",
+    "business_hours": "7:00-20:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "008",
+    "name": "vending machine",
+    "location": "Floor G, Forward Building A",
+    "average_cost_mop": "12",
+    "recommended_dishes": "Beverages, Snacks",
+    "business_hours": "all day",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "009",
+    "name": "vending machine",
+    "location": "1/F, Forward Building A",
+    "average_cost_mop": "8",
+    "recommended_dishes": "Beverages, Snacks",
+    "business_hours": "all day",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "010",
+    "name": "vending machine",
+    "location": "Floor G, Forward Building B",
+    "average_cost_mop": "15",
+    "recommended_dishes": "Beverages, Snacks",
+    "business_hours": "all day",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "011",
+    "name": "vending machine",
+    "location": "Floor G, PM Multisport Pavilion",
+    "average_cost_mop": "16",
+    "recommended_dishes": "Beverages, Snacks",
+    "business_hours": "all day",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "012",
+    "name": "Starlight Bookstore",
+    "location": "Floor G, Mingde Building",
+    "average_cost_mop": "50",
+    "recommended_dishes": "cake, coffee, snacks",
+    "business_hours": "weekdays:10:00-21:00; weekend:11:00-20:00",
+    "scene": "Coffee/Snack"
+  },
+  {
+    "id": "013",
+    "name": "Meng Tak Canteen",
+    "location": "1/F, Meng Tak Building, Macao Main Campus",
+    "average_cost_mop": "35",
+    "recommended_dishes": "Rice with Toppings, Clay Pot Rice",
+    "business_hours": "08:00 – 21:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "014",
+    "name": "Wui Chi Canteen",
+    "location": "1/F, Wui Chi Building, Macao Main Campus",
+    "average_cost_mop": "38",
+    "recommended_dishes": "Noodle Dishes",
+    "business_hours": "08:00 – 20:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  },
+  {
+    "id": "015",
+    "name": "Pearl Jubilee Canteen",
+    "location": "LG5 Entrance lobby, Pearl Jubilee Building, Taipa Campus",
+    "average_cost_mop": "42",
+    "recommended_dishes": "Snacks and Light Meals",
+    "business_hours": "08:00 – 20:00",
+    "scene": "Breakfast/Lunch/Dinner"
+  }
+]
